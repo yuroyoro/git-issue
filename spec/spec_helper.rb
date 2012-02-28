63d6ee437b96dc0966a9b8fc7c3fb885859bb4c8
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2012-02-28 14:11:14 +0900

    Fix old rspec configure.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'git_issue'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'rspec/autorun'
 
-Spec::Runner.configure do |config|
-
+RSpec.configure do |config|
 end
